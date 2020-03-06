bf242b9cca4d2583771038a42027798cf5536e0e
muyutingfeng <muyutingfeng@yahoo.com>
2020-03-06 22:14:56 +0800

    BubbleSort

--- src/main/java/com/github/mf/datastructure/sort/BubbleSortTest.java
+++ src/main/java/com/github/mf/datastructure/sort/BubbleSortTest.java
@@ -1,25 +1,36 @@
-package com.github.mf;
+package com.github.mf.datastructure.sort;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class BubbleSortTest {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 2, 7, 5, 6, 9, 0};
+        //int[] arr = {1, 2,3,4,5,6,7,8,9};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     public static void bubbleSort(int[] arr) {
+        boolean flag = false;
         int temp = 0;
         //控制轮次
         for (int i = 0; i < arr.length; i++) {
+            //控制每趟比较数量
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    flag = true;
                     temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
             }
+            //在一趟排序中一次交换都没有发生过
+            if (!flag) {
+                break;
+            } else {
+                flag = false;
+            }
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
